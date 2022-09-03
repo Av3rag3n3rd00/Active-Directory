7d3edae9acc3109c5f4ae5b251539240373e008f
Ashton Smith <averagenerd00gaming@gmail.com>
2022-09-03 11:19:42 -0700

    Updated

--- Code/gen_ad.ps1
+++ Code/gen_ad.ps1
@@ -1,4 +1,8 @@
-param( [Parameter(Mandatory=$true)] $JSONFile )
+param( 
+    [Parameter(Mandatory=$true)] $JSONFile,
+    [switch]$Undo
+ )
+
 $json = (Get-Content $JSONFile | ConvertFrom-Json )
 $Global:Domain = $json.domain
 
@@ -8,6 +12,14 @@ function CreateADGroup {
     $name = $groupObject.name
     New-ADGroup -name $name -GroupScope Global
 }
+
+function RemoveADGroup(){
+    param( [Parameter(Mandatory=$true)] $groupObject )
+
+    $name = $groupObject.name
+    Remove-ADGroup -Identity $name -Confirm:$False
+}
+
 function CreateADUser {
     param ( [Parameter(Mandatory=$true)] $userObject)
     
@@ -38,11 +50,48 @@ function CreateADUser {
 
 }
 
-foreach ($group in $json.groups) {
-    CreateADGroup $group
+function RemoveADUser(){
+    param( [Parameter(Mandatory=$true)] $userObject )
+
+    $name = $userObject.name
+    $firstname, $lastname = $name.Split(" ")
+    $username = ($firstname[0] + $lastname).ToLower()
+    $samAccountName = $username
+    Remove-ADUser -Identity $samAccountName -Confirm:$False
 }
 
-foreach ($user in $json.users) {
-    CreateADUser $user
+function WeakenPasswordPolicy(){
+    secedit /export /cfg C:\Windows\Tasks\secpol.cfg
+    (Get-Content C:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0").replace("MinimumPasswordLength = 7", "MinimumPasswordLength = 1") | Out-File C:\Windows\Tasks\secpol.cfg
+    secedit /configure /db c:\windows\security\local.sdb /cfg C:\Windows\Tasks\secpol.cfg /areas SECURITYPOLICY
+    Remove-Item -force C:\Windows\Tasks\secpol.cfg -confirm:$false
+}
+
+function StrengthenPasswordPolicy(){
+    secedit /export /cfg C:\Windows\Tasks\secpol.cfg
+    (Get-Content C:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 0", "PasswordComplexity = 1").replace("MinimumPasswordLength = 1", "MinimumPasswordLength = 7") | Out-File C:\Windows\Tasks\secpol.cfg
+    secedit /configure /db c:\windows\security\local.sdb /cfg C:\Windows\Tasks\secpol.cfg /areas SECURITYPOLICY
+    Remove-Item -force C:\Windows\Tasks\secpol.cfg -confirm:$false
+}
+
+if ( -not $Undo) {
+    WeakenPasswordPolicy
+
+    foreach ( $group in $json.groups ){
+        CreateADGroup $group
+    }
+    
+    foreach ( $user in $json.users ){
+        CreateADUser $user
+    }
+}else{
+    StrengthenPasswordPolicy
+
+    foreach ( $user in $json.users ){
+        RemoveADUser $user
+    }
+    foreach ( $group in $json.groups ){
+        RemoveADGroup $group
+    }
 }
 
--- Code/run.ps1
+++ Code/run.ps1
@@ -1,7 +1,12 @@
+param (
+    [int] $Groups,
+    [int] $Users
+)
+
 Import-Module CredentialManager.psm1
 $cred = Get-StoredCredential -name DomainController
 $dc = New-PSSession 192.168.220.155 -Credential $cred
-Start-Process powershell randomize.ps1 100 10
+.\randomize.ps1 AD_Schema.json -UserCount $Users -GroupCount $Groups
 Start-Sleep -Seconds .5
 Copy-Item gen_ad.ps1 -ToSession $dc C:\Windows\Tasks
 Copy-Item AD_Schema.json -ToSession $dc C:\Windows\Tasks
@@ -9,5 +14,5 @@ Start-Sleep -Seconds .5
 Enter-PSSession $dc
 Invoke-command -Session $dc -Scriptblock {
     Set-Location C:\Windows\Tasks
-    .\gen_ad.ps1 AD_Schema.json
+        .\gen_ad.ps1 AD_Schema.json
  }
